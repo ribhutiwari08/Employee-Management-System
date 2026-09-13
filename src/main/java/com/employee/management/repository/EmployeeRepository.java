2bd70255433a9ae917318ebb6970fffcd66ebf6d
Ribhu Bhushan Tiwari <ribhubhushan08@gmail.com>
2026-09-13 23:06:44 +0530

    Add employee repository

---
+++ src/main/java/com/employee/management/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.employee.management.repository;
+
+import com.employee.management.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
