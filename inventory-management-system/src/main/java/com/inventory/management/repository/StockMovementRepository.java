6bad54e799fcd29c0d9305c92c582f4f60c4de33
Ribhu Bhushan Tiwari <ribhubhushan08@gmail.com>
2026-09-14 00:17:57 +0530

    Add stock movement repository

---
+++ inventory-management-system/src/main/java/com/inventory/management/repository/StockMovementRepository.java
@@ -0,0 +1,9 @@
+package com.inventory.management.repository;
+
+import com.inventory.management.model.StockMovement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface StockMovementRepository extends JpaRepository<StockMovement, Long> {
+    List<StockMovement> findByProductIdOrderByCreatedAtDesc(Long productId);
+}
